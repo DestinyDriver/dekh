0dee33444ac20fed806a5b2e70998bbe120e035a
DestinyDriver <destinydriverx@gmail.com>
2026-01-02 19:37:16 +0530

    Build Simple Api Stuff

---
+++ app/api/search/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(req) {
+  console.log("API KEY:", process.env.TMDB_API_READ_ACCESS_TOKEN);
+  //convert the string first to url object
+  const url = new URL(req.url);
+  //to get the value of the search params query
+  const query = url.searchParams.get(`query`);
+
+  //to prevent edge case
+  if (!query || query.trim() == "") {
+    return NextResponse.json({ result: [] });
+  }
+
+  const fetchData = async () => {
+    //must encode the string to uri  i.e
+    //URLs break if they contain spaces, &, =, ?, etc.
+    //encodeURIComponent replaces them with encoded values.
+    //hello world & JS ==> "hello%20world%20%26%20JS"
+    const data = await axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_API_READ_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return data.data;
+  };
+
+  const result = await fetchData();
+  return NextResponse.json({ result: result });
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,62 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const page = () => {
-  return <div>page</div>;
+  // configDotenv();
+  const [search, setsearch] = useState("");
+
+  // Debouncing the Search Results to minimise the API CALLS to TMDB
+  useEffect(() => {
+    //to get rid of the empty call
+    if (search == "" || search.trim() == "") return;
+
+    const timerID = setTimeout(async () => {
+      await GetSearchResult();
+    }, 500);
+
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [search]);
+
+  // Function to set the moive name equals to search result
+  function changeSearchName(movie) {
+    setsearch(movie);
+  }
+
+  //To Fetch the Search Results on query by User
+  async function GetSearchResult() {
+    try {
+      const data = await axios.get(`/api/search?query=${search}`);
+      console.log("Data:");
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // function
+
+  // return (
+  //   <div className="flex justify-center items-center h-[100vh] bg-green-400">
+  //     <input
+  //       className="center h-20 w-40 bg-gray-600 pl-1 text-center"
+  //       onChange={(e) => {
+  //         changeSearchName(e.target.value);
+  //       }}
+  //     ></input>
+  //     <br></br>
+  //     <br></br>
+  //     <iframe
+  //       src="https://www.2embed.cc/embed/1593311"
+  //       height={500}
+  //       width={500}
+  //     ></iframe>
+  //   </div>
+  // );
+
+  return <div>hello</div>;
 };
 
 export default page;
